117eace77c0d105cb4366bcc73eb0193cb64c7ba
chersan <chernushevich.alexander@gmail.com>
2026-06-08 11:21:22 +0700

    vercel

--- apps/personal-fe/src/app/app.ts
+++ apps/personal-fe/src/app/app.ts
@@ -1,14 +1,12 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
   encapsulation: ViewEncapsulation.Emulated,
 })
 export class App {
-  protected title = 'personal-fe';
 }
